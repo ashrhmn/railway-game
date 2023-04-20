40b33427c7f590fb8ef729a3c043ff8e2aad5dea
Ashik Rahman <ashrhmn99@gmail.com>
2023-04-21 03:52:15 +0600

    fix zod error parsing

--- apps/api/src/filters/all-exception.filter.ts
+++ apps/api/src/filters/all-exception.filter.ts
@@ -1,4 +1,4 @@
-import { Catch, ArgumentsHost } from "@nestjs/common";
+import { Catch, ArgumentsHost, HttpException } from "@nestjs/common";
 import { BaseExceptionFilter } from "@nestjs/core";
 import { ZodError } from "zod";
 import { fromZodError } from "zod-validation-error";
@@ -6,27 +6,27 @@ import { fromZodError } from "zod-validation-error";
 @Catch()
 export class AllExceptionsFilter extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    // console.log({ exception });
-    if (
-      exception instanceof Error &&
-      exception.message === "Forbidden resource" &&
-      !host.switchToHttp().getRequest().route.methods.get
-    ) {
-      host.switchToHttp().getResponse().status(403).json({
-        statusCode: 403,
-        message: "You are not authorized to perform this action",
-        timestamp: new Date().toISOString(),
-      });
-    } else if (exception instanceof ZodError) {
-      host
-        .switchToHttp()
-        .getResponse()
-        .status(400)
-        .json({
-          statusCode: 400,
-          message: fromZodError(exception).toString(),
+    if (exception instanceof HttpException) {
+      if (
+        exception.message === "Forbidden resource" &&
+        !host.switchToHttp().getRequest().route.methods.get
+      ) {
+        host.switchToHttp().getResponse().status(403).json({
+          statusCode: 403,
+          message: "You are not authorized to perform this action",
           timestamp: new Date().toISOString(),
         });
+      } else if (exception.cause instanceof ZodError) {
+        host
+          .switchToHttp()
+          .getResponse()
+          .status(400)
+          .json({
+            statusCode: 400,
+            message: fromZodError(exception.cause).message,
+            timestamp: new Date().toISOString(),
+          });
+      }
     }
     super.catch(exception, host);
   }
